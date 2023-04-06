023a474a3032b129596601e16ae40094f6fb4373
N. P. Maling <npmaling@gmail.com>
2023-04-06 08:34:05 -0700

    Update copyright and license terms for each real file.

--- src/glngDB_SQLite_20220522.sql
+++ src/glngDB_SQLite_20220522.sql
@@ -1,4 +1,4 @@
--- Copyright 2013 N. P. Maling
+-- Copyright 2023 N. P. Maling
 -- 
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
+
 CREATE TABLE activity (
     activityid INT NOT NULL,
     projectid INT NOT NULL,
