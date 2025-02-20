70eea2fcfb31068c11878d71aa12d79b862a944a
N. P. Maling <npmaling@gmail.com>
2025-02-20 10:24:04 -0800

    minor updates and re-tries

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,7 @@ use crate::import::search_file_line_by_line;
 mod import;
 
 fn main() {
-//    search_file_line_by_line("/Users/npmal/projects/glngimport/ged");
-//    search_file_line_by_line("/Users/npmal/projects/glngimport/FS-20240512.ged");
-//    search_file_line_by_line("/Users/npmal/projects/glngimport/FS_FindaGraveIndex.ged");
-//    search_file_line_by_line("/Users/npmal/projects/glngimport/Maling_20230807.ged");
-    search_file_line_by_line("/Users/npmal/projects/glngimport/complete.ged");
+    search_file_line_by_line("/Users/npmal/projects/glngimport/Ancestry-data.ged");
+//    search_file_line_by_line("/Users/npmal/projects/glngimport/complete.ged");
 
 } // main
